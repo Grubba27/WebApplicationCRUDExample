18823aeb1aa9136eb44b5fb8237759ad03b9fd75
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-04-03 16:03:58 -0300

    cleaned user serivce

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -22,18 +22,28 @@ public class UserService
             library.Value.UsersCollectionName);
     }
 
-    public async Task<List<User>> GetUserAsync() =>
-        await _usersCollection.Find(_ => true).ToListAsync();
+    public async Task<List<User>> GetUserAsync()
+    {
+        return await _usersCollection.Find(_ => true).ToListAsync();
+    }
 
-    public async Task<User?> GetUserByIdAsync(string id) =>
-        await _usersCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+    public async Task<User?> GetUserByIdAsync(string id)
+    {
+        return await _usersCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+    }
 
-    public async Task CreateUserAsync(User newUser) =>
+    public async Task CreateUserAsync(User newUser)
+    {
         await _usersCollection.InsertOneAsync(newUser);
+    }
 
-    public async Task UpdateUserAsync(string id, User updatedUser) =>
+    public async Task UpdateUserAsync(string id, User updatedUser)
+    {
         await _usersCollection.ReplaceOneAsync(x => x.Id == id, updatedUser);
+    }
 
-    public async Task RemoveUserAsync(string id) =>
+    public async Task RemoveUserAsync(string id)
+    {
         await _usersCollection.DeleteOneAsync(x => x.Id == id);
+    }
 }
